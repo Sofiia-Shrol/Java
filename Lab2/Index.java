13665db0bca2c96696bc29786ed584f7ff124621
Sofiia Shrol <shrol.sofiya@gmail.com>
2024-12-11 16:11:52 +0200

    git commit -m "Add initial implementation for Lab2 and README"

---
+++ Lab2/Index.java
@@ -0,0 +1,10 @@
+package Lab2;
+
+public class Index {
+    public static void main(String[] args) {
+        Algebra result = new Algebra();
+        System.out.println("The result of exercise 14 is: " + result.math14(1.54, 0.49, 24.1, 0.87));
+        System.out.println("The result of exercise 5 is: " + result.math5(2.54, 1.23, -2.14, -0.23));
+        System.out.println("The result of exercise 23 is: " + result.math23(-3.45, -2.34, 1.45, 0.83));
+    }
+}
